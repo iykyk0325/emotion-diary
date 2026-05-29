7eb56e0f25ab4ae2118052a0461bcb6853dc48bf
Hoyoung Baek <iykyk0325@gmail.com>
2026-05-29 19:42:57 +0900

    feat: set routing, font, image, layout

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+
+import Diary from "./pages/Diary";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
