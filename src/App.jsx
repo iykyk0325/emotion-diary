11433293b86e30ccaef9b878751874f13bb60150
Hoyoung Baek <iykyk0325@gmail.com>
2026-05-30 02:57:20 +0900

    feat: Home 페이지 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,27 @@ import NotFound from "./pages/NotFound";
 const mockData = [
   {
     id: 0,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2026-04-26").getTime(),
     emotionId: 1,
-    content: "1번 일기 내용",
+    content: "0번 일기 내용",
   },
   {
     id: 1,
     createdDate: new Date().getTime(),
     emotionId: 2,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date("2026-06-26").getTime(),
+    emotionId: 3,
     content: "2번 일기 내용",
+  },
+  {
+    id: 3,
+    createdDate: new Date("2026-05-17").getTime(),
+    emotionId: 4,
+    content: "3번 일기 내용",
   },
 ];
 
@@ -37,8 +49,8 @@ const reducer = (state, action) => {
   }
 };
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 const App = () => {
   const [data, dispatch] = useReducer(reducer, mockData);
--- src/components/DiaryItem.jsx
+++ src/components/DiaryItem.jsx
@@ -1,22 +1,37 @@
 import "./DiaryItem.css";
 
+import { useNavigate } from "react-router-dom";
+
 import { getEmotionImage } from "../util/get-emotion-image";
 import Button from "./Button";
 
-const DiaryItem = () => {
-  const emotionId = 5;
+const DiaryItem = ({ id, emotionId, createdDate, content }) => {
+  const nav = useNavigate();
+
+  const goDiaryPage = () => {
+    nav(`/diary/${id}`);
+  };
+
+  const goEditPage = () => {
+    nav(`/edit/${id}`);
+  };
 
   return (
     <div className="DiaryItem">
-      <div className={`img_section img_section_${emotionId}`}>
+      <div
+        className={`img_section img_section_${emotionId}`}
+        onClick={goDiaryPage}
+      >
         <img src={getEmotionImage(emotionId)} />
       </div>
-      <div className="info_section">
-        <div className="created_date">{new Date().toLocaleDateString()}</div>
-        <div className="content">일기 컨텐츠</div>
+      <div className="info_section" onClick={goDiaryPage}>
+        <div className="created_date">
+          {new Date(createdDate).toLocaleDateString()}
+        </div>
+        <div className="content">{content}</div>
       </div>
       <div className="button_section">
-        <Button text="수정하기" />
+        <Button text="수정하기" onClick={goEditPage} />
       </div>
     </div>
   );
